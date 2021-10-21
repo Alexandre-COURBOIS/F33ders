cbbed52177b0852931041551836424832c5680bd
plasilva1997 <72378847+plasilva1997@users.noreply.github.com>
2021-10-21 14:46:31 +0200

    put  navbar in header component

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -11,39 +11,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    gsap.from('.logo', 1, {
-      delay: 1.2,
-      opacity: 0,
-      x: -20,
-      ease: Expo.easeInOut
-    })
-
-    gsap.from('.menu-links ul li', 1, {
-      delay: 1.2,
-      opacity: 0,
-      x: -20,
-      ease: Power4.easeInOut
-    })
-
-    gsap.from('.search', 1, {
-      delay: 1.6,
-      opacity: 0,
-      x: -20,
-      ease: Expo.easeInOut
-    })
-
-    gsap.from('.cart', 1, {
-      delay: 1.8,
-      opacity: 0,
-      x: -20,
-      ease: Expo.easeInOut
-    })
-
-    gsap.to('.border-bottom', 1, {
-      delay: 1.4,
-      width: '100%',
-      ease: Expo.easeInOut
-    })
 
     gsap.from('.subtitle', 1, {
       delay: 2.8,
