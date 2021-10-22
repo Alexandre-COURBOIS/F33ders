b1fe04bce15a290bc152df4d6e977b53cc331e9e
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-10-22 16:39:36 +0200

    working on header error on research

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {gsap} from "gsap";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ToastrService} from "ngx-toastr";
 import {SummonerService} from "../../Services/summoner.service";
+import {EncryptService} from "../../Services/encrypt.service";
 
 @Component({
   selector: 'app-header',
@@ -14,14 +15,27 @@ export class HeaderComponent implements OnInit {
   getPlayerForm !: FormGroup;
   submitted = false;
   timeExec = false;
+  token: any;
+  isLogged: any;
+
 
   constructor(
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
     private summonerService: SummonerService,
-  ) { }
+    private encryptService: EncryptService
+  ) {
+  }
 
   ngOnInit(): void {
+
+    this.isLogged = sessionStorage.getItem("_logged");
+
+    if (this.isLogged) {
+      this.isLogged = this.encryptService.decode(this.isLogged);
+    }
+
+    this.token = sessionStorage.getItem('_token');
 
     this.initSearchPlayerForm();
 
@@ -72,60 +86,72 @@ export class HeaderComponent implements OnInit {
 
     if (this.getPlayerForm.valid) {
 
-      this.submitted = true;
-      this.timeExec = true;
+      if (this.token && this.isLogged) {
+        this.submitted = true;
+        this.timeExec = true;
 
-      const surname = this.getPlayerForm.get('username')?.value;
+        const surname = this.getPlayerForm.get('username')?.value;
 
-      if (surname) {
+        if (surname) {
 
-        this.summonerService.setDataIntoDb(surname).subscribe(value => {
+          this.summonerService.setDataIntoDb(surname).subscribe(value => {
 
-          if (value.statusCode === 400) {
-            this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
-            this.timeExec = false;
-            this.submitted = false;
-          }
-
-          if (JSON.stringify(value).length > 50000) {
-            this.submitted = false;
-            localStorage.setItem('summoner', JSON.stringify(value));
-            sessionStorage.setItem('summoner', JSON.stringify(value));
-
-            if (value) {
+            if (value.statusCode === 400 || value.statusCode === 404) {
+              this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
               this.timeExec = false;
               this.submitted = false;
             }
 
-            this.toastr.success('Les données du joueur ' + surname + ' sont maintenant disponibles');
-          }
 
-          if (value === "Data has been set succesfully") {
-            this.summonerService.setDataIntoDb(surname).subscribe(value1 => {
-
-              this.timeExec = false;
+            if (JSON.stringify(value).length > 50000) {
               this.submitted = false;
-
-              localStorage.setItem('summoner', JSON.stringify(value1));
-              sessionStorage.setItem('summoner', JSON.stringify(value1));
+              localStorage.setItem('summoner', JSON.stringify(value));
+              sessionStorage.setItem('summoner', JSON.stringify(value));
 
               this.toastr.success('Les données du joueur ' + surname + ' sont maintenant disponibles');
 
-            }, error => {
-              this.timeExec = false;
-              this.submitted = false;
-              this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
-            });
-          }
-        }, error1 => {
+              if (value) {
+                this.timeExec = false;
+                this.submitted = false;
+              }
+            }
+
+            if (value === "Data has been set succesfully") {
+
+              this.summonerService.setDataIntoDb(surname).subscribe(value1 => {
+
+                if (value.statusCode === 400 || value.statusCode === 404) {
+                  this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
+                  this.timeExec = false;
+                  this.submitted = false;
+                } else {
+                  this.timeExec = false;
+                  this.submitted = false;
+
+                  localStorage.setItem('summoner', JSON.stringify(value1));
+                  sessionStorage.setItem('summoner', JSON.stringify(value1));
+
+                  this.toastr.success('Les données du joueur ' + surname + ' sont maintenant disponibles');
+                }
+
+              }, error => {
+                this.timeExec = false;
+                this.submitted = false;
+                this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
+              });
+            }
+          }, error1 => {
+            this.timeExec = false;
+            this.submitted = false;
+            this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
+          });
+        } else {
           this.timeExec = false;
           this.submitted = false;
-          this.toastr.error("Nous ne disposons d'aucun match correspond à ce profil");
-        });
+          this.toastr.error("Aucun nom d'invocateur n'est renseigné !");
+        }
       } else {
-        this.timeExec = false;
-        this.submitted = false;
-        this.toastr.error("Aucun nom d'invocateur n'est renseigné !");
+        this.toastr.error("Afin d'utiliser cette fonctionnalité veuillez vous connecter");
       }
     }
   }
